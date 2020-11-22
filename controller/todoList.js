66eff5e6d52a2fca4c71d3f859da5b8c689a6e33
sthaweechat <sthaweechat@gmail.com>
2020-11-22 22:30:09 +0700

    first commit

---
+++ controller/todoList.js
@@ -0,0 +1,45 @@
+const { uniqueId } = require("lodash");
+
+let todoList = [];
+
+
+const getTodoList = (req, res) => {
+    res.status(200).send(todoList);
+
+};
+
+
+const createTodoList = (req, res) => {
+    const newTodo = {
+        id: uniqueId(),
+        task: req.body.task
+    }
+    todoList.push(newTodo);
+    res.status(201).send(newTodo);
+
+};
+
+const updateTodoList = (req, res) => {
+    const targetId = String(req.params.id);
+    const targetIdx = todoList.findIndex(todo => todo.id === targetId);
+    todoList[targetIdx] = {
+        id: targetId,
+        task: req.body.task,
+    }
+    res.status(200).send({ message: "Updateing is success" });
+};
+
+const deleteTodoList = (req, res) => {
+    const targetId = String(req.params.id);
+    todoList = todoList.filter(todo => todo.id !== targetId);
+    res.status(204).send();
+
+}
+
+module.exports = {
+    createTodoList,
+    getTodoList,
+    updateTodoList,
+    deleteTodoList
+};
+
